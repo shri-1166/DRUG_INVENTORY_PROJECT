ea2d0455448eab9216c5a33c98bc34126d526e7f
shri-1166 <shriid1166@gmail.com>
2025-08-02 13:43:03 +0530

    Delete drug-inventory-management-system directory

--- drug-inventory-management-system/src/main/java/drug/inventory/management_system/repository/SupplierRepository.java
+++
@@ -1,9 +0,0 @@
-package drug.inventory.management_system.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import drug.inventory.management_system.Entity.Supplier;
-
-public interface SupplierRepository extends JpaRepository<Supplier, Long> {
-
-}
--- drug-inventory-management-system/src/main/java/drug/inventory/management_system/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package drug.inventory.management_system.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import drug.inventory.management_system.Entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	User findByEmail(String email);
-}
